1c4af5ac0ef498f3fbe2d4c0024ae26383388c72
Santo La Rocca <laroccasantoandrea@gmail.com>
2024-10-10 09:55:47 +0200

    asda

---
+++ src/main/java/com/example/esTestUnit/EsTestUnitApplication.java
@@ -0,0 +1,13 @@
+package com.example.esTestUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsTestUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsTestUnitApplication.class, args);
+	}
+
+}
